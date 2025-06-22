62ad0e95d3e4fac6a740a90c69b487b1b4e26596
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2025-06-22 12:40:50 -0300

    Initial commit

---
+++ src/test/java/com/softwaresolutions/crudclients/CrudclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaresolutions.crudclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
